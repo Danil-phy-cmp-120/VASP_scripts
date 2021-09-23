dc8b506f021d51d4d15ce5cc3deb034c86fff083
Danil-phy-cmp-120 <51433598+Danil-phy-cmp-120@users.noreply.github.com>
2021-09-23 11:13:42 +0500

    Add files via upload

--- segregation_test.py
+++ segregation_test.py
@@ -8,8 +8,8 @@ import itertools
 
 num = 3 # Число соединений на которые распадается исходный сплав
 
-all_reac = {'Ni7Co1Mn7In1': -113.98884} # Все энергии должны быть приведены на число атомов указанном ключах словарей
-all_prod = {'Co1': -7.1083, 'Co3Ni1': -27.2084, 'In1': -2.7517, 'In3Co1': -15.416, 'In7Ni3': -38.02, 'In1Ni1': -8.8814, 'Mn1': -9.1617, 'Mn3Co1': -34.6588, 'Mn1Co1': -16.322, 'Mn1Ni3': -26.9488, 'Ni1': -5.7798, 'Mn2In1Co1': -27.3588, 'Mn1In1Ni2': -23.8236, 'Mn8Ni8': -122.427}
+all_reac = {'Mn2Sc1Si1': -61.9275297725} # Все энергии должны быть приведены на число атомов указанном ключах словарей
+all_prod = {'Mn2Sc1Si2': -74.20357883, 'Mn3Si1': -66.33381478, 'Mn4Si4': -116.95593121, 'Mn8Sc4': -203.82809295, 'Mn29': -528.15173, 'Sc2': -28.4978581, 'Sc2Si2': -51.90588478, 'Sc10Si6': -215.2864648, 'Si2': -20.0152677, 'Mn16Si28': -589.01413457, 'Mn6Sc4Si2': -190.6439151, 'Mn12Sc12Si24': -661.56152365} # Даже если атом один после него необходимо ставить "1"
 
 #################################################################################################################################################################################################
 
@@ -24,6 +24,8 @@ def find_sum_str(S):
             ans += last_number
             last_number = 0
     return(ans + last_number)
+    
+print('{} atoms in reac'.format(find_sum_str(list(all_reac.keys())[0])))
 
 data = []
 for i in itertools.combinations(all_prod.keys(), num):
@@ -31,16 +33,14 @@ for i in itertools.combinations(all_prod.keys(), num):
         reac, prod = balance_stoichiometry(all_reac.keys(), i)
         if (sum(1 for number in  prod.values() if number < 0)) == 0:
         
-            E_formation = all_reac[list(all_reac.keys())[0]]
-            reactions = '{} ->'.format(list(all_reac.keys())[0])
+            E_formation = all_reac[list(all_reac.keys())[0]]*reac[(list(reac.keys())[0])]
+            reactions = '{} {} ->'.format(reac[list(all_reac.keys())[0]], list(all_reac.keys())[0])
 
             for j in prod.keys():
                 E_formation -= prod[j] * all_prod[j] 
-                reactions += ' {} +'.format(j)
-                
-            reactions[:len(reactions)-2]    
-            E_formation = E_formation/find_sum_str(list(all_reac.keys())[0]) 
+                reactions += ' {} {} +'.format(prod[j], j)    
             
+            E_formation = E_formation/(reac[list(reac.keys())[0]] * find_sum_str(list(all_reac.keys())[0]))
             data += [[reactions[:len(reactions)-2], E_formation]]
                  
     except:
