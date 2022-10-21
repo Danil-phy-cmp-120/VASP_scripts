7a43395ce2322fd62e20114948ba4bfb895697c5
Danil-phy-cmp-120 <51433598+Danil-phy-cmp-120@users.noreply.github.com>
2022-10-21 13:51:17 +0500

    Add files via upload

--- read_DOS_1.py
+++ read_DOS_1.py
@@ -6,18 +6,17 @@ import os
 import matplotlib.pyplot as plt
 import matplotlib.ticker as ticker
 
+
 e_min = -2
 e_max = 2
+n = 4
 
 plot_total = bool(input('Do plot TDOS (True/False)\n').split())
 
 sites = input('Enter the numbers of atoms for pDOS\n').split()
 sites = np.array(sites, dtype = int)
 
-
-if not os.path.isdir('DOS'):
-    os.makedirs('DOS')
-      
+colors = ["#ed0062","#4300ed","#00d37b","#8400e2","#c5f700"]
 
 xml = Vasprun('vasprun.xml')
 
@@ -29,12 +28,14 @@ if xml.converged:
 
     if plot_total == True:
         t_energies = xml.tdos.energies - xml.tdos.efermi
-        t_dos_up = xml.tdos.get_densities(Spin(1))
-        t_dos_down = -xml.tdos.get_densities(Spin(-1))
+        t_dos_up = xml.tdos.get_densities(Spin(1))/n
+        t_dos_down = -xml.tdos.get_densities(Spin(-1))/n
         t_data = np.column_stack((t_energies, t_dos_up, t_dos_down))
 
         t_condition = ((t_data[:,0]>e_min) & (t_data[:,0]<e_max))
-        ax.fill_between(t_data[t_condition,0], t_data[t_condition,1], t_data[t_condition,2], alpha=0.5, label = 'Total')
+        ax.fill_between(t_data[t_condition,0], t_data[t_condition,1], t_data[t_condition,2], alpha=0.5, label = 'Total', color = colors[0])
+        ax.plot(t_data[t_condition,0], t_data[t_condition,1], alpha=1, color = colors[0])
+        ax.plot(t_data[t_condition,0], t_data[t_condition,2], alpha=1, color = colors[0])
         
         ax.plot([e_min, e_max], [0.0, 0.0], '--', linewidth=0.8, color='black')#Ноли
         ax.plot([0.0, 0.0], [min(t_data[t_condition,2]), max(t_data[t_condition,1])], '--', linewidth=1, color='black') #Ноли
@@ -42,7 +43,7 @@ if xml.converged:
  
     ax.text(0.1, 0.1, '{:.0f} %'.format(xml.complete_dos.spin_polarization*100), horizontalalignment='center', verticalalignment='center', transform=ax.transAxes, fontsize = 18)
         
-
+    n_colors = 1
     for s in sites:
         energies = xml.complete_dos.get_site_dos(xml.structures[-1][s]).energies - xml.complete_dos.get_site_dos(xml.structures[-1][s]).efermi
         dos_up = xml.complete_dos.get_site_dos(xml.structures[-1][s]).get_densities(Spin(1))
@@ -52,7 +53,10 @@ if xml.converged:
                         
         condition = ((data[:,0]>e_min) & (data[:,0]<e_max))
                         
-        ax.fill_between(data[condition,0], data[condition,1], data[condition,2], alpha=0.5, label = xml.structures[-1][s].species)
+        ax.fill_between(data[condition,0], data[condition,1], data[condition,2], alpha=0.5, label = xml.structures[-1][s].specie, color = colors[n_colors])
+        ax.plot(data[condition,0], data[condition,1], alpha=1, color = colors[n_colors])
+        ax.plot(data[condition,0], data[condition,2], alpha=1, color = colors[n_colors])
+        n_colors += 1
         
         if plot_total == False:
             ax.plot([e_min, e_max], [0.0, 0.0], '--', linewidth=0.8, color='black')#Ноли
@@ -70,6 +74,25 @@ if xml.converged:
                        #title_fontsize = 20   #  размер шрифта заголовка
                             )
     legend.get_title().set_fontsize('14')
+    
+    ax.tick_params(axis='both',  # Применяем параметры к обеим осям
+                   which='major',  # Применяем параметры к вспомогательным делениям
+                   direction='in',  # Рисуем деления внутри и снаружи графика
+                   # length = 10,    #  Длинна делений
+                   # width = 2,     #  Ширина делений
+                   # color = 'm',    #  Цвет делений
+                   #pad=10,  # Расстояние между черточкой и ее подписью
+                   labelsize=14,  # Размер подписи
+                   labelcolor='k',  # Цвет подписи
+                   bottom=True,  # Рисуем метки снизу
+                   top=True,  # сверху
+                   left=True,  # слева
+                   right=True,  # и справа                   
+                   labelbottom = True,    #  Рисуем подписи снизу
+                   labeltop = False,    #  сверху
+                   labelleft = True,    #  слева
+                   labelright = False,    #  и справа
+                   labelrotation = 0)    #  Поворот подписей)  # и справа
                         
     fig.savefig('DOS.png', transparent = False, bbox_inches = 'tight')
                         
