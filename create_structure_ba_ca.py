fbb5e83c443fbd37fd93a7c724c0bbb34902161c
Erager <92916399+Erager@users.noreply.github.com>
2022-11-03 12:50:06 +0500

    Update create_structure_ba_ca.py

--- create_structure_ba_ca.py
+++ create_structure_ba_ca.py
@@ -4,15 +4,15 @@ import pymatgen.io.vasp.inputs as inputs
 import os
 import shutil
 
-potentials = {'Fe':'Fe', 'Ge':'Ge_d'}
+potentials = {'In':'In_d', 'Mn':'Mn_pv', 'Ni':'Ni_pv'}
 
 path_a0 = input('pathion\n')
 
 for i in np.arange(0.85, 1.15, 0.05):
     for j in np.arange(0.85, 1.15, 0.05):
     
-        if not os.path.isdir('ba_ca_{}/{:.3f}/{:.3f}'.format(path_a0, i, j)):
-            os.makedirs('ba_ca_{}/{:.3f}/{:.3f}'.format(path_a0, i, j))
+        if not os.path.isdir('ba_ca/{:.3f}/{:.3f}'.format(i, j)):
+            os.makedirs('ba_ca/{:.3f}/{:.3f}'.format(i, j))
 
         structure = Structure.from_file('{}/CONTCAR'.format(path_a0))
            
@@ -27,33 +27,32 @@ for i in np.arange(0.85, 1.15, 0.05):
         paw = [potentials[s.symbol] for s in structure.types_of_species]
         potcar = inputs.Potcar(symbols=paw, functional="PBE_54")
                       
-        kpoints = inputs.Kpoints.automatic_density(structure, 4000)
+        kpoints = inputs.Kpoints.automatic_density(structure, 5000)
 
         incar = inputs.Incar({'LCHARG' : False,
                                    'LWAVE' :  False,
                                    'ICHARG' : 1,
                                    'IBRION' : 2,
-                                   'ISIF' : 2,
+                                   'ISIF' : 7,
                                    'POTIM' : 0.1,
                                    'PREC' : 'High',
-                                   'ENCUT' : 410,
                                    'EDIFF' : 1e-8,
-                                   'EDIFFG' : -1e-2,
+                                   'EDIFFG' : 1e-3,
                                    'ISMEAR' : 1,
                                    'SIGMA' : 0.1,
                                    'ISPIN' : 2,
                                    'LMAXMIX' : 6,
                                    'ADDGRID' : False,
                                    'LORBIT' : 10,
-                                   'LASPH' : True,
+                                   'LASPH' : False,
                                    'NSW ': 100,
                                    'NEDOS' : 1000,
                                    'NELM' : 101,
                                    'ALGO' : 'Normal',
                                    'NCORE' : 12,
                                    '#KPAR' : 4,
-                                   'MAGMOM': '14*3 2*0'})
+                                   'MAGMOM': '16*0.4 8*3.5 4*-3.5 4*0.04'})
                        
                        
         VaspInput = inputs.VaspInput(incar, kpoints, poscar, potcar)
-        VaspInput.write_input('ba_ca_{}/{:.3f}/{:.3f}'.format(path_a0, i, j))
+        VaspInput.write_input('ba_ca/{:.3f}/{:.3f}'.format(i, j))
